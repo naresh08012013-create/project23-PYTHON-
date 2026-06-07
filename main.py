6d6d9b657e581ff7e499ca837fa98c10673ea2c4
Legendry Guy <naresh08012013@gmail.com>
2026-06-07 19:19:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+Set1 = {'blue', 'green'} 
+Set2 ={'blue', 'yellow'}
+print("Set1",Set1)
+print("Set2",Set2)
+set3 = Set1.intersection(Set2)
+print("symmetric difference",set3)
+
+Set1 = {1, 2, 3, 4, 5} 
+Set2 = {1, 5,6, 7, 8, 9}
+print("Set1",Set1)
+print("Set2",Set2)
+set3 = Set1.intersection(Set2)
+print ("symmetric difference",set3)
